78ac96e94ad2835bfea2596c2cebd0d042449a02
Francesco Penasa <francesco.penasa.job@gmail.com>
2022-02-20 10:47:52 +0100

    add modularization

--- main.cpp
+++ main.cpp
@@ -6,8 +6,20 @@
 
 using namespace std;
 
+void algorithm (string input, string output);
+
 int main()
 {
+    algorithm("input/a_an_example.in.txt", "output/a_an_example.out.txt" );
+    algorithm("input/b_basic.in.txt", "output/b_basic.out.txt" );
+    algorithm("input/c_coarse.in.txt", "output/c_coarse.out.txt" );
+    algorithm("input/d_difficult.in.txt", "output/d_difficult.out.txt" );
+    algorithm("input/e_elaborate.in.txt", "output/e_elaborate.out.txt" );
+
+    return 0;
+}
+
+void algorithm(string input, string output) {
     int C; // clients
     int L; // liked ingredients
     int D; // disliked ingredients
@@ -17,7 +29,7 @@ int main()
     set<string> liked;
     set<string> disliked;
 
-    ifstream in("input/a_an_example.in.txt");
+    ifstream in(input);
     in >> C;
 
     for (int i = 0; i<C*2; i++) {
@@ -37,11 +49,9 @@ int main()
 
     L = liked.size();
 
-    ofstream out("output/a_an_example.out.txt");
+    ofstream out(output);
     out << L << " ";
     for (set<string>::iterator it = liked.begin(); it != liked.end(); it++){
         out << *it << " ";
-    }   
-
-    return 0;
+    }  
 }
